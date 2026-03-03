2c991313cfb9d2825e94d6ea91666e3ad71df7c9
boyang <liboyang@zspace.cn>
2026-03-03 15:42:25 +0800

    feat: init project with springboot3, mybatis-plus and rest-assured

---
+++ src/main/java/ink/testpulse/TestPulseApplication.java
@@ -0,0 +1,13 @@
+package ink.testpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPulseApplication.class, args);
+    }
+
+}
